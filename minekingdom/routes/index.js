493d3db24faaf1eacd8154099268a6ef586b73e5
Damien Vesper <ldamienvesper@gmail.com>
2020-01-05 19:12:10 -0600

    Update files.

--- minekingdom/routes/index.js
+++ minekingdom/routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET pages. */
 router.get(`/`, (req, res, next) => res.render(`index`, { title: `MineKingdom` }));
 router.get(`/about`, (req, res, next) => res.render(`about`, { title: `About - MineKingdom` }));
-router.get(`/docs`, (req, res, next) => res.render(`docs`, { title: `Documentation - MineKingdom` }));
+router.get(`/commands`, (req, res, next) => res.render(`commands`, { title: `Commands - MineKingdom` }));
 router.get(`/team`, (req, res, next) => res.render(`team`, { title: `Meet the Team - MineKingdom` }));
 router.get(`/contact`, (req, res, next) => res.render(`contact`, { title: `Contact - MineKingdom` }));
 
